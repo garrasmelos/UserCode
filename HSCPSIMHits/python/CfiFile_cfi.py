7a7f478a868437e55e7b5c572e06819bae144109
Gabriel Ramirez <gabriel.ramirez.sanchez@cern.ch>
2017-03-23 16:36:45 +0100

    Federica's modification on SIMHits handler

--- HSCPSIMHits/python/CfiFile_cfi.py
+++ HSCPSIMHits/python/CfiFile_cfi.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 demo = cms.EDAnalyzer('HSCPSIMHits',
-                     hitsLabel = cms.InputTag("g4SimHits","MuonRPCHits")
-)
+                      hitsLabel = cms.InputTag("g4SimHits","MuonRPCHits"),
+                      genEventInfo = cms.InputTag("generator"),
+                      hepEventInfo = cms.InputTag("generator")
+                      )
