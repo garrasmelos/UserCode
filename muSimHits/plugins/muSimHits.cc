09a73d99374311f45ed7fb31fcd1c08c0986b1b3
Gabriel Ramirez <gabriel.ramirez.sanchez@cern.ch>
2017-06-22 18:07:00 +0200

    muSimHit reading

--- muSimHits/plugins/muSimHits.cc
+++ muSimHits/plugins/muSimHits.cc
@@ -1,9 +1,9 @@
 // -*- C++ -*-
 //
-// Package:    test/muSimHits
+// Package:    HSCPAnalysis/muSimHits
 // Class:      muSimHits
 // 
-/**\class muSimHits muSimHits.cc test/muSimHits/plugins/muSimHits.cc
+/**\class muSimHits muSimHits.cc HSCPAnalysis/muSimHits/plugins/muSimHits.cc
 
  Description: [one line class summary]
 
@@ -28,6 +28,17 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "SimDataFormats/TrackingHit/interface/PSimHit.h"
+#include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+#include <DataFormats/MuonDetId/interface/RPCDetId.h>
+#include <DataFormats/MuonDetId/interface/CSCDetId.h>
+#include <DataFormats/MuonDetId/interface/GEMDetId.h>
+#include <DataFormats/MuonDetId/interface/DTLayerId.h>
+#include "DataFormats/MuonDetId/interface/MuonSubdetId.h"
+#include "DataFormats/DetId/interface/DetId.h"
 //
 // class declaration
 //
@@ -43,15 +54,19 @@ class muSimHits : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
       explicit muSimHits(const edm::ParameterSet&);
       ~muSimHits();
 
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-
-
    private:
       virtual void beginJob() override;
       virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
       virtual void endJob() override;
 
       // ----------member data ---------------------------
+      
+      edm::EDGetTokenT<edm::PSimHitContainer> simHitRPCtoken_;
+      edm::EDGetTokenT<edm::PSimHitContainer> simHitDTtoken_;
+      edm::EDGetTokenT<edm::PSimHitContainer> simHitCSCtoken_;
+      edm::EDGetTokenT<edm::PSimHitContainer> simHitGEMtoken_;
+      edm::EDGetTokenT<std::vector<reco::GenParticle>> genParToken_;
+      int pId_;
 };
 
 //
@@ -66,7 +81,12 @@ class muSimHits : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
 // constructors and destructor
 //
 muSimHits::muSimHits(const edm::ParameterSet& iConfig)
-
+: simHitRPCtoken_(consumes<edm::PSimHitContainer>(iConfig.getParameter<edm::InputTag>("simHitRPCLabel"))),
+  simHitDTtoken_(consumes<edm::PSimHitContainer>(iConfig.getParameter<edm::InputTag>("simHitDTLabel"))),
+  simHitCSCtoken_(consumes<edm::PSimHitContainer>(iConfig.getParameter<edm::InputTag>("simHitCSCLabel"))),
+  simHitGEMtoken_(consumes<edm::PSimHitContainer>(iConfig.getParameter<edm::InputTag>("simHitGEMLabel"))),
+  genParToken_(consumes<std::vector<reco::GenParticle>>(iConfig.getParameter<edm::InputTag>("genParticleLabel"))),
+  pId_(iConfig.getParameter<int>("particleId"))
 {
    //now do what ever initialization is needed
    usesResource("TFileService");
@@ -91,19 +111,67 @@ muSimHits::~muSimHits()
 void
 muSimHits::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+  using namespace edm;
+  using namespace std;
 
+  Handle<std::vector<reco::GenParticle>> genParticlesHandle;
+  iEvent.getByToken(genParToken_, genParticlesHandle);
 
+  Handle<PSimHitContainer> simHitsRPCHandle;
+  iEvent.getByToken(simHitRPCtoken_, simHitsRPCHandle);
 
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
+  Handle<PSimHitContainer> simHitsDTHandle;
+  iEvent.getByToken(simHitDTtoken_, simHitsDTHandle);
+
+  Handle<PSimHitContainer> simHitsCSCHandle;
+  iEvent.getByToken(simHitCSCtoken_, simHitsCSCHandle);
+
+  Handle<PSimHitContainer> simHitsGEMHandle;
+  iEvent.getByToken(simHitGEMtoken_, simHitsGEMHandle);
+
+  if(genParticlesHandle.isValid())
+  {
+    for(auto& genParticle : *genParticlesHandle)
+    {
+      cout << genParticle.p4().Eta() << endl; 
+    }
+  }
+  
+  
+  if(simHitsRPCHandle.isValid())
+  {
+    for(auto& simHitRPC : *simHitsRPCHandle)
+    {
+      
+      int particleId = simHitRPC.particleType();
+      if( particleId != pId_) continue;
+      
+      DetId detId = DetId(simHitRPC.detUnitId());
+      if(detId.subdetId() == MuonSubdetId::RPC)
+      {
+        RPCDetId rollId(detId);
+        cout << simHitRPC.particleType() << endl;
+        cout << rollId.station() << endl;
+        cout << "is iRPC: " << roll->isIRPC();
+      }
+    }
+  }
+
+  if(simHitsDTHandle.isValid())
+  {
+    for(auto& simHitDT : *simHitsDTHandle)
+    {
+      int particleId = simHitDT.particleType();
+      if(particleId != pId_) continue;
+
+      DetId detId= DetId(simHitDT.detUnitId());
+      if(detId.subdetId() == MuonSubdetId::DT)
+      {
+        DTLayerId layerId(detId);
+        cout << "DT: " << dtLayerId.station() <<endl;
+      }
+    }
+  }
 }
 
 
@@ -117,16 +185,6 @@ muSimHits::beginJob()
 void 
 muSimHits::endJob() 
 {
-}
-
-// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
-void
-muSimHits::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
 }
 
 //define this as a plug-in
