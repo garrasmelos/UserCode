0e3b75214a373b62314738b6a23f4b79cb0c2405
Gabriel Ramirez <gabriel.ramirez.sanchez@cern.ch>
2017-06-28 17:16:35 +0200

    HSCPSIMHits update

--- HSCPSIMHits/python/CfiFile_cfi.py
+++ HSCPSIMHits/python/CfiFile_cfi.py
@@ -2,6 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 demo = cms.EDAnalyzer('HSCPSIMHits',
                       hitsLabel = cms.InputTag("g4SimHits","MuonRPCHits"),
-                      genEventInfo = cms.InputTag("generator"),
-                      hepEventInfo = cms.InputTag("generator")
+                      genParticleLabel = cms.InputTag("genParticles")
                       )
--- HSCPSIMHits/test/hcsp_simhits.py
+++ HSCPSIMHits/test/hcsp_simhits.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
-import FWCore.Utilities.FileUtils as FileUtils
+#import FWCore.Utilities.FileUtils as FileUtils
 
 process = cms.Process("Demo")
 
-mylist = FileUtils.loadListFromFile('list.txt')
+#mylist = FileUtils.loadListFromFile('list.txt')
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
@@ -13,19 +13,20 @@ process.MessageLogger.cerr.INFO = cms.untracked.PSet(
 )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.load("Geometry.MuonCommonData.muonIdealGeometryXML_upscope_cfi")
-process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
-process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
-process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.Geometry.GeometryExtended2023D12_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D12Reco_cff')
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
-    #fileNames = cms.untracked.vstring(
+    fileNames = cms.untracked.vstring(
     #     '/store/group/dpg_rpc/comm_rpc/Sandbox/garamire/HSCP_MC_GEN-SIM-Jan2017/HSCP_MC_GEN-SIM-Jan2017/170124_092652/0000/step1_1.root',
-    fileNames = cms.untracked.vstring (*mylist)
-    #)
+    #fileNames = cms.untracked.vstring (*mylist)
+          'file:/afs/cern.ch/user/g/garamire/work/private/HSCP_stau-m1599_NoPU/HSCPppstau_M_1599_NoPU.root',
+    )
 )
 
-process.load('iHSCPAnalysis.HSCPSIMHits.CfiFile_cfi')
+process.load('HSCPAnalysis.HSCPSIMHits.CfiFile_cfi')
 process.TFileService = cms.Service("TFileService",
              fileName = cms.string("hTest.root")
 )
