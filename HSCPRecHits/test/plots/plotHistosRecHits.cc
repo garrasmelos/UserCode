4a73b6d32172a35da85192ee5414a119e3b9adf4
Gabriel Ramirez <gabriel.ramirez.sanchez@cern.ch>
2017-07-26 11:44:09 +0200

    update

--- HSCPRecHits/test/plots/plotHistosRecHits.cc
+++ HSCPRecHits/test/plots/plotHistosRecHits.cc
@@ -1,11 +1,12 @@
 #include "plotHistosRecHits.h"
+#include <fstream>
 void plotEff( TEfficiency * eff)
 {
 	setStyleTemplate();
   int iPeriod = 100;    // 1=7TeV, 2=8TeV, 3=7+8TeV, 4=13TeV, 7=7+8+13TeV 
   int iPos    = 11;
   
-  TCanvas * canvEff = new TCanvas("canvEff", "canvEff", 1000, 1000);
+  TCanvas * c1Eff = new TCanvas("c1Eff", "c1Eff", 1000, 1000);
   TString lumi_13TeV_MC = "MC Pythia6 - #tilde{#tau}(M1599 GeV) of GMSB model" ;
   eff->SetTitle(";#beta_{GEN};Efficiency");
   eff->SetMarkerColor(kGreen+2);
@@ -18,35 +19,100 @@ void plotEff( TEfficiency * eff)
   legEff->SetLineColor(1);
   legEff->SetBorderSize(0);
   legEff->Draw();
-  CMS_lumi( canvEff, iPeriod, iPos );
-  canvEff->SaveAs("trigEff-HSCPtrigger-OnlySlopeCut.pdf");
+  CMS_lumi( c1Eff, iPeriod, iPos );
+  c1Eff->SaveAs("trigEff-HSCPtrigger-OnlySlopeCut.pdf");
+  c1Eff->SaveAs("trigEff-HSCPtrigger-OnlySlopeCut.png");
 }
 
+vector<double > fitWithErrors(int nhits, double hitTime[20],double hitTimeErr[20],double hitPos[20],double hitPosErr[20])
+{
+  vector <double> results;
+  double hitPos_tmp[nhits],hitPosErr_tmp[nhits],hitTime_tmp[nhits],hitTimeErr_tmp[nhits];
+  int nHits=0;
+  for(int j = 0; j< nhits; j++)
+  {
+    bool repeated=false;
+    bool causal=true;
+    for(int k =0; k<j; k++)
+    { 
+      if(hitPos[j]==hitPos[k] ) repeated = true;
+      if(hitTime[k]>hitTime[j] ) causal=false;
+    }
+    if(!repeated && causal) 
+    {
+      hitPos_tmp[nHits]=hitPos[j];
+      hitTime_tmp[nHits]=hitTime[j];
+      hitPosErr_tmp[nHits]=hitPosErr[j];
+      hitTimeErr_tmp[nHits]=hitTimeErr[j]; 
+      nHits++;
+    }
+  }  
+  TF1 * poly1 = new TF1("poly1","[0]+[1]*x", 4., 15.);
+  poly1->SetParLimits(0,-950.,100.);
+  poly1->SetParLimits(1,-10., 10.);
+  poly1->SetParameters(0.0, 0.1);
+  TGraphErrors *gr = new TGraphErrors(nhits,hitPos,hitTime,hitPosErr,hitTimeErr); 
+
+  gr->Fit("poly1");
+  double p0 = poly1->GetParameter(0);
+  double p0Err = poly1->GetParError(0);
+  double p1 = poly1->GetParameter(1);
+  double p1Err = poly1->GetParError(1);
+  
+  results.push_back(p0);
+  results.push_back(p0Err);
+  results.push_back(p1);
+  results.push_back(p1Err);
+  //if(p0 < -950)
+  //{
+  //  gr2->Fit("poly1","W");
+  //  gr2->Draw();
+  //  c1->SaveAs("FitPlot.pdf");
+  //  
+  //  return 0;
+  //}
+  return results; 
+}
 int plotHistosRecHits()
 {
 	setStyleTemplate();
   setBranches();
+  ofstream outfile("file.txt");
   styleTemplate->SetOptStat("nemruo");
   TString infile = "HSCP_RecHits.root";
   TString infile2 = "HSCP_MuTrigger_RecHits.root";
   TString infile3 = "ZMM_RecHits.root";
-    
+  
+  double tmin= -100.; 
+  double tmax= 100.;
   TFile* fin = new TFile(infile);
   TFile* fin2 = new TFile(infile2);
   TFile* fin3 = new TFile(infile3);
   
-  TCanvas * c1 = new TCanvas("c1", "c1", 1000, 1000);
+  int W = 1000;
+  int H = 1000;
+  float T = 0.08*H;
+  float B = 0.12*H; 
+  float L = 0.12*W;
+  float R = 0.04*W;
+  TCanvas * c1 = new TCanvas("c1", "c1",50,50,W,H);
   TH1D *fHBeta_pas = new TH1D("fHBeta_pas","fHBeta_pas",50,0.,1.);
   TH1D *fHBeta_tot =  new TH1D("fHBeta_tot","fHBeta_tot",50,0.,1.);
   TH1D *fHBeta_pas_BetaErrorCut = new TH1D("fHBeta_pas_BetaErrorCut","fHBeta_pas_BetaErrorCut",50,0.,1.);
   TH1D *fHBeta_pas_SlopeCut = new TH1D("fHBeta_pas_SlopeCut","fHBeta_pas_SlopeCut",50,0.,1.); 
   TH1D *fHBeta_resolution = new TH1D("fHBeta_resolution","fHBeta_resolution", 60, -3.,3.); 
-  TH1D *fHt0 = new TH1D("fHt0","fHt0",50,-25., 25.); 
+  TH1D *fHBeta_resolution_bx = new TH1D("fHBeta_resolution_bx","fHBeta_resolution_bx", 60, -3.,3.); 
+  TH1D *fHt0 = new TH1D("fHt0","fHt0",50,tmin,tmax); 
   TH1D *fHtime = new TH1D("fHtime","fHtime", 60, -10.,50.);
-  TH1D *fHtimeNewFit = new TH1D("fHtimeNewFit","fHtimeNewFit",50, -25.,25.);
+  TH1D *fHtimeNewFit = new TH1D("fHtimeNewFit","fHtimeNewFit",50, tmin,tmax);
+  TH1D *fHtimeNewFit2 = new TH1D("fHtimeNewFit2","fHtimeNewFit2",50, tmin,tmax);
   TH1D *fHEta_pas = new TH1D("fHEta_pas", "fHEta_pas", 50,-3.15,3.15); 
   TH1D *fHEta_tot = new TH1D("fHEta_tot","fHEta_tot", 50,-3.15,3.15); 
   TH1D *fHbx = new TH1D("fHbx","fHbx", 300, -150., 150.); 
+
+  TH2D *fHrpcBetaVsGenEta_pas = new TH2D("fHrpcBetaVsGenEta_pas","fHrpcBetaVsGenEta_pas",20, 0., 1.,10, 0.,2.0);
+  TH2D *fHrpcBetaVsGenEta_tot = new TH2D("fHrpcBetaVsGenEta_tot","fHrpcBetaVsGenEta_tot",20, 0., 1.,10, 0.,2.0);
+  TH2D *fHrpcBetaVsGenEta_eff = new TH2D("fHrpcBetaVsGenEta_eff","fHrpcBetaVsGenEta_eff",20, 0., 1.,10, 0.,2.0);
   
   TH1D * fHBeta_MuTrig_pas = (TH1D*) fin2->Get("demo2/fHbeta_pas");
   TH1D * fHBeta_MuTrig_tot = (TH1D*) fin2->Get("demo2/fHbeta_tot");
@@ -57,7 +123,7 @@ int plotHistosRecHits()
   
   unsigned short rpcHits_n, rpcHits_n_mu;
   Double_t rpcHitTime[20], rpcHitTime_mu[20],rpcHitTimeErr[20],rpcHitPos[20],rpcHitPosErr[20];
-  double rpcBeta, rpcBetaErr, t0, t0_bx, genBeta, genEta,fitSlope;
+  double rpcBeta, rpcBetaErr,rpcBeta_bx, t0, t0_bx, genBeta, genEta,fitSlope;
   double rpcBeta_mu, rpcBetaErr_mu, t0_mu, t0_bx_mu, genBeta_mu, genEta_mu,fitSlope_mu;
   TTree *tree = (TTree*)fin->Get("demo2/tree");
   TTree *tree_mu = (TTree*)fin3->Get("demo2/tree");
@@ -77,6 +143,7 @@ int plotHistosRecHits()
   tree->SetBranchAddress("rpcHitPos", rpcHitPos);
   tree->SetBranchAddress("rpcHitPosErr", rpcHitPosErr);
   tree->SetBranchAddress("rpcBeta",&rpcBeta); 
+  tree->SetBranchAddress("rpcBeta_bx",&rpcBeta_bx); 
   tree->SetBranchAddress("rpcBetaErr",&rpcBetaErr);
   tree->SetBranchAddress("t0",&t0);
   tree->SetBranchAddress("t0_bx",&t0_bx);
@@ -90,12 +157,13 @@ int plotHistosRecHits()
   TString lumi_8TeV  = "19.1 fb^{-1}"; // default is "19.7 fb^{-1}"
   TString lumi_7TeV  = "4.9 fb^{-1}";  // default is "5.1 fb^{-1}"
   int iPeriod = 100;    // 1=7TeV, 2=8TeV, 3=7+8TeV, 4=13TeV, 7=7+8+13TeV 
-  int iPos    = 11;
-  TString lumi_13TeV_MC = "MC Pythia6 - #tilde{#tau}(M1599 GeV) of GMSB model" ;
+  int iPos    = 0;
+  int iPosR    = 33;
+  //TString lumi_13TeV_MC = "MC Pythia6 - #tilde{#tau}(M1599 GeV) of GMSB model" ;
 
   //fin = new TFile(infile);
-  //fin2 = new TFile(infile2);
-  //fin3 = new TFile(infile3);
+    //fin2 = new TFile(infile2);
+    //fin3 = new TFile(infile3);
 
   Long64_t nEntries = tree->GetEntries();
   Long64_t nEntries_mu = tree_mu->GetEntries();
@@ -103,15 +171,21 @@ int plotHistosRecHits()
   for(Long64_t i = 0 ; i < nEntries ; i++)
   {
     tree->GetEntry(i);
-    if(rpcHits_n >2)
+    if(rpcHits_n >=4 )
     {
+      vector<double> fitRes = fitWithErrors(rpcHits_n, rpcHitTime,rpcHitTimeErr,rpcHitPos,rpcHitPosErr);
+      fHtimeNewFit->Fill(fitRes[0]);
+      double res = (genBeta-rpcBeta)/genBeta; 
+      fHBeta_resolution->Fill(res);
+      double res_bx = (genBeta-rpcBeta_bx)/genBeta;
+      fHBeta_resolution_bx->Fill(res_bx);
       if(fitSlope > 0 && rpcBetaErr/rpcBeta < 0.3 && rpcBeta<0.7)
       {
-        double res = (genBeta-rpcBeta)/genBeta; 
-        fHBeta_resolution->Fill(res);
         fHBeta_pas->Fill(genBeta);
         fHEta_pas->Fill(genEta);
+        fHrpcBetaVsGenEta_pas->Fill(genBeta, TMath::Abs(genEta));
       }
+      fHrpcBetaVsGenEta_tot->Fill(genBeta, TMath::Abs(genEta));
       
       if(rpcBetaErr/rpcBeta < 0.3 && rpcBeta<0.7)
       {
@@ -126,23 +200,6 @@ int plotHistosRecHits()
       fHt0->Fill(t0);
       fHbx->Fill(t0_bx);
       //cout << rpcHitPos[1] << " " << rpcHits_n <<  endl;
-      TF1 * poly1 = new TF1("poly1","[0]+[1]*x", 4., 15.);
-      poly1->SetParLimits(0,-100.,100.);
-      poly1->SetParLimits(1,-10., 10.);
-      poly1->SetParameters(0.0, 0.1);
-      TGraphErrors *gr = new TGraphErrors(rpcHits_n,rpcHitPos,rpcHitTime,rpcHitPosErr,rpcHitTimeErr); 
-      gr->Fit("poly1");
-      double p0 = poly1->GetParameter(0);
-      cout << p0 << endl; 
-      fHtimeNewFit->Fill(p0);
-    //  if(p0 > 1000)
-    //  {
-    //    gr->Fit("pol1","W");
-    //    gr->Draw();
-    //    c1->SaveAs("FitPlot.pdf");
-    //    
-    //    return 0;
-    //  }
     }
     for(UShort_t k=0 ; k < rpcHits_n ; k++)
     {
@@ -165,34 +222,106 @@ int plotHistosRecHits()
       fHtime_Zmm->Fill(rpcHitTime_mu[k]);
     }
   }
+  c1->SetFillColor(0);
+  c1->SetBorderMode(0);
+  c1->SetFrameFillStyle(0);
+  c1->SetFrameBorderMode(0);
+  c1->SetLeftMargin( L/W );
+  c1->SetRightMargin( R/W );
+  c1->SetTopMargin( T/H );
+  c1->SetBottomMargin( B/H );
+  c1->SetTickx(0);
+  c1->SetTicky(0);
+  //c1->SetRightMargin(.12);
+  styleTemplate->SetOptStat(0);
+  styleTemplate->SetPalette(51);
+  fHrpcBetaVsGenEta_pas->Sumw2();
+  fHrpcBetaVsGenEta_tot->Sumw2();
+  fHrpcBetaVsGenEta_pas->Divide(fHrpcBetaVsGenEta_tot);
+  fHrpcBetaVsGenEta_pas->GetXaxis()->SetTitle("#beta");
+  fHrpcBetaVsGenEta_pas->GetYaxis()->SetTitle("|#eta|");
+  c1->SetRightMargin( .15 ); 
+  fHrpcBetaVsGenEta_pas->Draw("colz");
+  CMS_lumi( c1, iPeriod, iPos );
+  c1->SaveAs("rpcBetaVsGenEta.pdf");
+  c1->SaveAs("rpcBetaVsGenEta.png");
+  c1->SetRightMargin( R/W );
   fHt0->Draw();
+  fHtimeNewFit->SetLineColor(kBlue);
   fHtimeNewFit->Draw("SAME");
+  fHtimeNewFit2->SetLineColor(kRed);
+  fHtimeNewFit2->Draw("SAME");
   c1->SaveAs("timeNewFit.pdf");
+  c1->SaveAs("timeNewFit.png");
 
-   TEfficiency* trigEff = new TEfficiency(*fHBeta_pas,*fHBeta_tot);
-   trigEff->SetTitle(";#beta_{GEN};Efficiency");
-   trigEff->SetMarkerColor(kRed+2);
-   trigEff->SetLineColor(kRed+2);
-   trigEff->SetName("trigEff");
+  TEfficiency* trigEff = new TEfficiency(*fHBeta_pas,*fHBeta_tot);
+  trigEff->SetTitle(";#beta_{GEN};Efficiency");
+  trigEff->SetMarkerColor(kRed+2);
+  trigEff->SetLineColor(kRed+2);
+  trigEff->SetName("trigEff");
+  
+  TEfficiency* muTrigEff = new TEfficiency(*fHBeta_MuTrig_pas,*fHBeta_MuTrig_tot);
+  muTrigEff->SetTitle(";#beta_{GEN};Efficiency");
+  muTrigEff->SetMarkerColor(kBlue+2);
+  muTrigEff->SetLineColor(kBlue+2);
+  muTrigEff->SetName("muTrigEff");
+  muTrigEff->Draw();
+  gPad->Update();
+  auto gMu = muTrigEff->GetPaintedGraph();
+  gMu->SetMinimum(0.);
+  gMu->SetMaximum(1.);
+  gMu->GetXaxis()->SetRangeUser(0.,1.);
+  gPad->Update();
+  TLegend *lg = new TLegend(0.12,0.80,0.42,0.87);
+  lg->AddEntry(muTrigEff,"HLT_L1SingleMuOpen_v3 trigger","lep");
+  lg->SetTextSize(0.03);
+  lg->SetFillColor(0);
+  lg->SetFillStyle(0);
+  lg->SetLineColor(1);
+  lg->SetBorderSize(0);
+  lg->Draw();
+  CMS_lumi( c1, iPeriod, iPos );
+  c1->SaveAs("trigEff-HSCPTrigger.pdf");
+  c1->SaveAs("trigEff-HSCPTrigger.png");
    
-   TEfficiency* muTrigEff = new TEfficiency(*fHBeta_MuTrig_pas,*fHBeta_MuTrig_tot);
-   muTrigEff->SetTitle(";#beta_{GEN};Efficiency");
-   muTrigEff->SetMarkerColor(kBlue+2);
-   muTrigEff->SetLineColor(kBlue+2);
-   muTrigEff->SetName("muTrigEff");
-   muTrigEff->Draw();
-   trigEff->Draw("SAME");
+  trigEff->Draw();
+  TLegend *lg2 = new TLegend(0.15,0.15,0.45,0.22);
+  lg2->AddEntry(trigEff,"HSCP trigger","lep");
+  lg2->SetTextSize(0.03);
+  lg2->SetFillColor(0);
+  lg2->SetFillStyle(0);
+  lg2->SetLineColor(1);
+  lg2->SetBorderSize(0);
+  lg2->Draw();
+  gPad->Update();
+  auto g = trigEff->GetPaintedGraph();
+  g->SetMinimum(0.);
+  g->SetMaximum(1.);
+  g->GetXaxis()->SetRangeUser(0.,1.);
+  gPad->Update();
 
-   TLegend *legend = new TLegend(0.45,0.15,0.85,0.3);
-   legend->AddEntry(trigEff,"HSCP trigger","lep");
-   legend->AddEntry(muTrigEff,"HLT_L1SingleMuOpen_v3 trigger","lep");
-   legend->SetTextSize(0.025);
-   //legend->SetTextColor();
-   legend->SetLineColor(1);
-   legend->SetBorderSize(0);
-   legend->Draw();
-   CMS_lumi( c1, iPeriod, iPos );
-   c1->SaveAs("trigEff-HSCPandMuTriggers.pdf");
+  CMS_lumi( c1, iPeriod, iPos );
+  c1->SaveAs("trigEff-MuTrigger.pdf");
+  c1->SaveAs("trigEff-MuTrigger.png");
+
+
+
+  muTrigEff->Draw();
+  trigEff->Draw("SAME");
+  TLegend *lg3 = new TLegend(0.13,0.65,0.4,0.72);
+  lg3->AddEntry(trigEff,"HSCP trigger","lep");
+  lg3->AddEntry(muTrigEff,"HLT_L1SingleMuOpen_v3 trigger","lep");
+  lg3->SetTextSize(0.03);
+  lg3->SetFillColor(0);
+  //lg3->SetFillStyle(0);
+  lg3->SetLineColor(1);
+  lg3->SetBorderSize(0);
+  lg3->Draw();
+  CMS_lumi( c1, iPeriod, iPos );
+  c1->SaveAs("trigEff-Mu-HSCPTriggers.pdf");
+  c1->SaveAs("trigEff-Mu-HSCPTriggers.png");
+
+
 
    TEfficiency* trigEff2 = new TEfficiency(*fHBeta_pas_BetaErrorCut,*fHBeta_tot);
    trigEff2->SetTitle(";#beta_{GEN};Efficiency");
@@ -209,6 +338,7 @@ int plotHistosRecHits()
    legend2->Draw();
    CMS_lumi( c1, iPeriod, iPos );
    c1->SaveAs("trigEff-HSCPtrigger-OnlyFitErrorCut.pdf"); 
+   c1->SaveAs("trigEff-HSCPtrigger-OnlyFitErrorCut.png"); 
    c1->cd();
 
    TEfficiency* trigEff3 = new TEfficiency(*fHBeta_pas_SlopeCut,*fHBeta_tot);
@@ -235,16 +365,32 @@ int plotHistosRecHits()
    legendEta->Draw();
    CMS_lumi( c1, iPeriod, iPos );
    c1->SaveAs("trigEffEta-HSCPtrigger.pdf");
+   c1->SaveAs("trigEffEta-HSCPtrigger.png");
 
 
 	fHBeta_resolution->GetXaxis()->SetTitle("#beta resolution (#beta_{GEN}-#beta_{RPC})/#beta_{GEN}");
 	fHBeta_resolution->SetLineColor(kGreen+2);
-	fHBeta_resolution->SetFillStyle(3444);
-	fHBeta_resolution->SetFillColor(kGreen+2);
-	fHBeta_resolution->Draw();
+  fHBeta_resolution->SetLineWidth(4);
+	fHBeta_resolution->DrawNormalized();
 	
-	CMS_lumi( c1, iPeriod, iPos );
+	fHBeta_resolution_bx->GetXaxis()->SetTitle("#beta resolution (#beta_{GEN}-#beta_{RPC})/#beta_{GEN}");
+	fHBeta_resolution_bx->SetLineColor(kOrange+2);
+	fHBeta_resolution_bx->SetLineWidth(4);
+	fHBeta_resolution_bx->DrawNormalized("SAME");
+  
+  TLegend *lgRes = new TLegend(0.15,0.55,0.4,0.64);
+  lgRes->AddEntry(fHBeta_resolution_bx,"Phase 1 - 25ns t res","f");
+  lgRes->AddEntry(fHBeta_resolution,"Phase 2 - 1ns t res","f");
+  lgRes->SetTextSize(0.03);
+  lgRes->SetFillColor(0);
+  lgRes->SetFillStyle(0);
+  lgRes->SetLineColor(1);
+  lgRes->SetBorderSize(0);
+  lgRes->Draw();
+	
+  CMS_lumi( c1, iPeriod, iPos );
 	c1->SaveAs("beta_GenRes.pdf");
+	c1->SaveAs("beta_GenRes.png");
   
   fHt0->GetXaxis()->SetTitle("Time [ns]");
   fHt0->SetLineColor(kAzure+1);
@@ -255,6 +401,8 @@ int plotHistosRecHits()
   fHbx->SetLineColor(kRed+1);
   fHbx->SetName("fHbx");
   fHbx->GetXaxis()->SetTitle("Time [ns]");
+  fHbx->SetLineWidth(4);
+  fHt0->SetLineWidth(4);
   fHbx->DrawNormalized();
   fHt0->DrawNormalized("SAME");
   TLegend *legendTime = new TLegend(0.28,0.7,0.42,0.8);
@@ -268,17 +416,41 @@ int plotHistosRecHits()
   legendTime->Draw();
   CMS_lumi( c1, iPeriod, iPos );
   c1->SaveAs("timeZeroAndBx.pdf");
+  c1->SaveAs("timeZeroAndBx.png");
   c1->SetLogy();
   c1->SaveAs("timeZeroAndBx_log.pdf");
+  c1->SaveAs("timeZeroAndBx_log.png");
   
   c1->SetLogy(0);
-  fHtime->GetXaxis()->SetTitle("Time [ns]");
+  fHtime_Zmm->GetXaxis()->SetTitle("Time [ns]");
   fHtime->SetLineColor(kPink+2);
-  fHtime->SetFillStyle(3444);
-  fHtime->SetFillColor(kPink+2);
-  fHtime->Draw();
+//  fHtime->SetFillStyle(3444);
+//  fHtime->SetFillColor(kPink+2);
+  fHtime_Zmm->SetLineColor(kBlue+2);
+  fHtime_Zmm->SetLineWidth(4);
+  fHtime->SetLineWidth(4);
+  fHtime_Zmm->DrawNormalized();
+  fHtime->DrawNormalized("SAME");
+  TLegend *lgTime = new TLegend(0.55,0.55,0.9,0.64);
+  lgTime->AddEntry(fHtime_Zmm,"Muon","f");
+  lgTime->AddEntry(fHtime,"#tilde{#tau}","f");
+  lgTime->SetTextSize(0.03);
+  lgTime->SetFillColor(0);
+  lgTime->SetFillStyle(0);
+  lgTime->SetLineColor(1);
+  lgTime->SetBorderSize(0);
+  lgTime->Draw();
   CMS_lumi( c1, iPeriod, iPos );
   c1->SaveAs("time.pdf");
+  c1->SaveAs("time.png");
+
+
+  fHBeta_Zmm_pas->Draw();
+  c1->SaveAs("beta_Zmm_pas.pdf");
+  c1->SaveAs("beta_Zmm_pas.png");
+  fHBeta_Zmm_tot->Draw();
+  c1->SaveAs("beta_Zmm_tot.pdf");
+  c1->SaveAs("beta_Zmm_tot.png");
    TEfficiency* trigEff_Zmm = new TEfficiency(*fHBeta_Zmm_pas,*fHBeta_Zmm_tot);
    trigEff_Zmm->SetTitle(";#beta_{GEN};Efficiency");
    trigEff_Zmm->SetMarkerColor(kBlack+2);
@@ -301,6 +473,7 @@ int plotHistosRecHits()
    legend4->Draw();
    CMS_lumi( c1, iPeriod, iPos );
    c1->SaveAs("trigEff_ZMM-HSCPtrigger.pdf");
+   c1->SaveAs("trigEff_ZMM-HSCPtrigger.png");
    c1->cd();
   c1->SetLogy(0);
   fHtime_Zmm->GetXaxis()->SetTitle("Time [ns]");
@@ -320,5 +493,6 @@ int plotHistosRecHits()
   fHtime_Zmm->GetXaxis()->SetRangeUser(-10.5,10.5);
   CMS_lumi( c1, iPeriod, iPos );
   c1->SaveAs("time_Zmm.pdf");
+  c1->SaveAs("time_Zmm.png");
 	return 0;
 }
